0466996caadf096c378012f64a37f06ffff91c17
Masayuki Onishi <omccello@gmail.com>
2020-10-13 22:39:38 +0900

    アイテム一覧を表示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:ver_1_by_sfc/ver_1_by_sfc.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -9,40 +14,77 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Future<List<Item>> fetchItemList() async {
+  final response = await rootBundle
+      .loadString('lib/extentions/ver_1_by_sfc/assets/data/item_list.json');
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  if (response.isNotEmpty) {
+    final jsonArray = json.decode(response).cast<Map<String, dynamic>>()
+        as List<Map<String, dynamic>>;
+
+    return jsonArray.map((json) => Item.fromJson(json)).toList();
+  } else {
+    return null;
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<List<Item>> items;
+
+  @override
+  void initState() {
+    super.initState();
+    items = fetchItemList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: Text('風来のシレン SFC版アイテム検索'),
+    );
+  }
+
+  Widget _buildBody() {
+    return Center(
+      child: Scrollbar(
+        child: _futureListView(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+    );
+  }
+
+  FutureBuilder<List<Item>> _futureListView() {
+    return FutureBuilder<List<Item>>(
+        future: items,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) => _itemTile(snapshot.data[index]),
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          } else {
+            return null;
+          }
+        });
+  }
+
+  Widget _itemTile(Item _item) {
+    return ListTile(
+      leading: ExcludeSemantics(
+        child: CircleAvatar(child: Text('${_item.id}')),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      title: Text(_item.name),
+      subtitle: Text('買値: ${_item.askPrice}  売値: ${_item.sellPrice}'),
     );
   }
 }
